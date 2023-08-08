c4747ae7703a926bbe55505479fa1a05f102433c
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-08-08 15:02:30 +0200

    Fix compilation error

--- src/wg/vec.cljs
+++ src/wg/vec.cljs
@@ -1,5 +1,5 @@
 (ns wg.vec
-  (:refer-clojure :exclude [+ - * /]))
+  (:refer-clojure :exclude [+ - * / div]))
 
 (def +' clojure.core/+)
 (def -' clojure.core/-)
@@ -20,7 +20,7 @@
   [(*' x scale) (*' y scale)])
 
 (defn / [[x1 y1] [x2 y2]]
-  [(/ x1 x2) (/y1 y2)])
+  [(/ x1 x2) (/ y1 y2)])
 
 (defn div [[x y] scale]
   [(div x scale) (div y scale)])
